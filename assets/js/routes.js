de38e6c53581969e3cdc49b7b5c6c221a8b08ebe
wenrenmuyuan <3106133216@qq.com>
2026-05-18 11:40:19 +0800

    feat: 添加 resume-md 跳转

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -2,5 +2,6 @@ window.ZWENRENMUYUAN_ROUTES = {
   '.': '/',
   github: 'https://github.com/Zwenrenmuyuan',
   'image-compressor': '/image-compressor/',
+  'resume-md': '/resume-md/',
   '404': '/404.html',
 };
